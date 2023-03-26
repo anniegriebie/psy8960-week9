bfb6798ad4ae33fce3c709fff38d6098c3e60fbf
anniegriebie <GRIEB046@umn.edu>
2023-03-26 16:09:46 -0500

    Changing headings

--- R/reddit_api.R
+++ R/reddit_api.R
@@ -1,9 +1,9 @@
-# Script Settings and Resources
+## Script Settings and Resources
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 library(tidyverse)
 library(jsonlite)
 
-# Data Import and Cleaning
+## Data Import and Cleaning
 #used fromJSON to read in content from reddit link because listed as a json file in reddit api documentation. 
 rstats_list <- fromJSON("https://www.reddit.com/r/rstats/.json", flatten = TRUE)
 
@@ -17,18 +17,18 @@ rstats_tbl <- rstats_original_tbl %>%
          comments = data.num_comments)
 
 
-#Visualizatoin
+## Visualizatoin
 #uses ggplot to display scatterplot
 ggplot(rstats_tbl, aes(x=upvotes, y=comments))+
   geom_smooth()+
   geom_point()
 
-#Analysis
+## Analysis
 #uses cor.test to calculate the correlation between upvotes and comements, creates an object that can be called later for displaying the publication data
 correlationAPI <- cor.test(rstats_tbl$upvotes, rstats_tbl$comments)
 
-#Publication
-"The correlation between upvotes and comments was r(23) = 0.34, p = 0.1. This test was not statistically significant."
+## Publication
+#"The correlation between upvotes and comments was r(23) = 0.34, p = 0.1. This test was not statistically significant."
 #displays the correlation to specifcations using the object created in previous step, setting the correct number of decimal places. uses paste0 because 
 paste0("The correlation between upvotes and comments was r", "(", correlationAPI$parameter[[1]] ,") = ",round(correlationAPI$estimate,2), ", p = ",  round(correlationAPI$p.value,2), ". This test was not statistically significant.")
 
