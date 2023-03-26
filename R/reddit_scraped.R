d77ec2c45d6b7af73d1d7ade9e0ee4b26d9409de
anniegriebie <GRIEB046@umn.edu>
2023-03-26 16:03:46 -0500

    generating text elements

--- R/reddit_scraped.R
+++ R/reddit_scraped.R
@@ -10,3 +10,26 @@ rstats_html <- read_html("https://old.reddit.com/r/rstats/")
 post_text <- rstats_html %>%
   html_elements(xpath = "//div[contains(@class, 'even') or contains(@class, 'odd')]//a[contains(@class, 'title may-blank')]") %>%
   html_text()
+
+#generating text elements using xpaths
+upvotes_text <- rstats_html %>%
+  html_elements(xpath = "//div[contains(@class, 'even') or contains(@class, 'odd')]//div[@class = 'score unvoted']")%>%
+  html_text()
+
+#generating text elements using xpaths
+comments_text <- rstats_html %>%
+  html_elements(xpath = "//div[contains(@class, 'even') or contains(@class, 'odd')]//div[@class = 'entry unvoted']//li[position() = 1]") %>%
+  html_text()
+
+#cleaning all of the text elements to go into tbl. 
+#post element is good as is
+post <- post_text 
+#upvotes need to be changed from character to numeric and also have NA values replaced with 0 to go into correlation
+upvotes <- upvotes_text %>%
+  as.numeric() %>%
+  replace_na(0)
+#comments needs to be changed from character to numeric and also have NA replaced with 0 to go into correlation
+comments <- comments_text%>%
+  str_extract(pattern = "\\d+") %>%
+  as.numeric() %>%
+  replace_na(0)
